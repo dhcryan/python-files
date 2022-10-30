ae8b3192239e0a18d551eebcd7c48daab12a02e9
Donghun Choi <dhc4003@gmail.com>
2022-10-30 19:41:04 +0900

    ANN

--- function.py
+++ function.py
@@ -1,10 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 인공 신경망 구현
+
 
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
 
-X = np.arange(-5.0, 5.0, 0.1)
-print(X)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# 항등함수
+
+
+def identity_function(x):
+    return x
+
+# softmax 함수
+
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
+
+# x = np.array([[1, 2], [3, 4]])
+# y = np.array([[5, 6], [7, 8]])
+# print(np.dot(x, y))
+
+# X = np.arange(-5.0, 5.0, 0.1)
+# Y = sigmoid(X)
+# plt.plot(X, Y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
